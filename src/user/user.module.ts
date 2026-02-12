ec738f5839c1f0322d2b93c2de6df89c9b7b34c9
KlayPeter <kt_mmxyy2377@qq.com>
2026-02-12 12:05:13 +0800

    feat: 添加jwt认证体系和用户管理

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,13 @@ import { UserService } from './user.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { ConsumptionRecord, ConsumptionRecordSchema } from 'src/interview/schemas/consumption-record.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      {name:User.name,schema:UserSchema}
+      {name:User.name,schema:UserSchema},
+      {name:ConsumptionRecord.name,schema:ConsumptionRecordSchema}
     ])
     , DatabaseModule],
   controllers: [UserController],
