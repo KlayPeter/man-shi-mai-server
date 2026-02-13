7ffeac25b8834016bdba08266405e7a4761fae1c
KlayPeter <kt_mmxyy2377@qq.com>
2026-02-13 11:37:15 +0800

    feat: 添加langChain分析报告功能

--- src/interview/interview.controller.ts
+++ src/interview/interview.controller.ts
@@ -1,6 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Request,
+  Res,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { InterviewService } from './services/interview.service';
 
 @Controller('interview')
 export class InterviewController {
-  constructor() {}
+  constructor(private readonly interviewService: InterviewService) { }
+  
+  @Post('/analyze-resume')
+  async analyzeResume(@Body() body: { resume: string;  jobDescription: string; }) {
+    const result = await this.interviewService.analyzeResume(body.resume, body.jobDescription);
+    return {
+      code: 200,
+      data:result
+    }
+
+  }
 }
--- src/interview/services/interview.service.ts
+++ src/interview/services/interview.service.ts
@@ -1,6 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { AIModelFactory } from 'src/ai/services/ai-model.factory';
+import {PromptTemplate} from '@langchain/core/prompts'
+import { JsonOutputParser } from '@langchain/core/output_parsers'
+import { ConfigService } from '@nestjs/config';
+import { RESUME_QUIZ_PROMPT } from '../prompts/resume-quiz.prompts';
+
 
 @Injectable()
 export class InterviewService {
-  constructor() {}
+  private readonly logger = new Logger(InterviewService.name);
+  constructor(
+    private configService: ConfigService,
+    private aiModelFactory: AIModelFactory,
+  ) { }
+  /**
+   * 分析简历并生成报告
+   * 
+   * @param resumeContent - 简历的文本内容
+   * @param jobDescription - 岗位要求
+   * @return - 分析报告
+   * */
+  async analyzeResume(resumeContent: string, jobDescription: string) {
+    // 创建Prompt模板
+    const prompt = PromptTemplate.fromTemplate(RESUME_QUIZ_PROMPT)
+    
+    // 通过工厂获取模型（而不是自己初始化
+    const model = this.aiModelFactory.createDefaultModel()
+
+    // 创建输出解析器
+    const parser = new JsonOutputParser()
+
+    // 创建链：Prompt -> Model -> OutputParser
+    const chain = prompt.pipe(model).pipe(parser)
+    
+    // 调用链
+    try {
+      this.logger.log(`开始分析简历...`)
+      const result = await chain.invoke({
+        resume_content: resumeContent,
+        job_description: jobDescription
+      })
+
+      this.logger.log(`简历分析完成`)
+      return result
+    } catch (error) {
+      this.logger.error(`简历分析失败: ${error}`)
+      throw error
+    }
+  }
 }
